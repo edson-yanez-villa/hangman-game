8917c5abd1bf2673202524c5d6aecb19a0bb90f2
Moises Edson Yañez Villa <yanezvilla.edson@gmail.com>
2022-09-06 23:27:53 -0400

    Se actualizo el metodo para renderizar las letras

--- controllers/game.js
+++ controllers/game.js
@@ -36,8 +36,6 @@ const renderWords = () => {
     const heightCanva = canvaLeters.clientHeight;
     const widthLine = widthCanva/(correctWord.length*1.5);
 
-    const em = canvaLeters.offsetHeight;
-
     let pincel = canvaLeters.getContext("2d");
 
     pincel.lineWidth = 5;
@@ -46,7 +44,7 @@ const renderWords = () => {
     
     let possitionLine = 0;
     correctWord.forEach(letter => {
-        pincel.fillText(letter, possitionLine+(widthLine/2) - 14, heightCanva/2 - 30);
+        pincel.fillText(letter, possitionLine + (widthLine/2) - 14, heightCanva/2 - 30);
         pincel.moveTo(possitionLine, heightCanva/2);
         pincel.lineTo(possitionLine + (widthLine*0.8),heightCanva/2);
         pincel.stroke();
